a49480a618f83f09491375c53046f1ed70c40336
syedjaferk <learnwithjafer@gmail.com>
2025-02-23 16:11:25 +0530

    feat: app routes to rendering in Server mode

--- tracker/src/app/app.routes.server.ts
+++ tracker/src/app/app.routes.server.ts
@@ -4,6 +4,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
